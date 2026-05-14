f11551a0bb37e319fcbe7f2bc057368ba5c86bc4
Shaqqaqi <ihmji88@gmail.com>
2026-05-13 23:48:04 -0700

    Add web script

---
+++ static/mywebscript.js
@@ -0,0 +1,14 @@
+function runEmotionAnalysis() {
+    let textToAnalyze = document.getElementById("textToAnalyze").value;
+
+    let xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 200) {
+            document.getElementById("system_response").innerHTML = xhttp.responseText;
+        }
+    };
+
+    xhttp.open("GET", "/sentimentAnalyzer?textToAnalyze=" + encodeURIComponent(textToAnalyze), true);
+    xhttp.send();
+}
